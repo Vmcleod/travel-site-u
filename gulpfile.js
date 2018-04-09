6b9d570d3eaddfa1360fa1da922aa4e2226bd369
Vallesa <vallesamcleod@gmail.com>
2018-04-09 16:51:58 -0500

    added more json packages and used postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,26 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    cssvars = require('postcss-simple-vars'),
+    nested = require('postcss-nested');
 
-gulp.task('default', function() {
+gulp.task('default', function () {
     console.log("Hooray - you created a gulp task.");
 });
 
-gulp.task('html', function() {
+gulp.task('html', function () {
     console.log("Imagine something useful being done to your HTML here.");
 });
 
 gulp.task('styles', function () {
-    console.log("Imagine SASS or PostCSS tasks running here.");
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssvars, nested, autoprefixer]))
+        .pipe(gulp.dest('./app/temp/styles.css'));
 });
 
 gulp.task('watch', function () {
-    watch('./app.index.html', function() {
+    watch('./app.index.html', function () {
         gulp.start('html');
     });
 
